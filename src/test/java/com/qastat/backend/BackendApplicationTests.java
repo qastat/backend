b1071cfd120a42fbfb22bc275e496011e5eb6d7e
chingiskhan.kalanov <chingiskhan.kalanov@aitupay.kz>
2025-04-28 12:11:40 +0500

    initial commit

---
+++ src/test/java/com/qastat/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qastat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
